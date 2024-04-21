ef70ff36c3d519e28573c0b47ae24c3b707b82cb
jgwentworth92 <alphabetboi92@protonmail.com>
2024-04-21 13:20:59 -0400

    added new routes

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,10 @@
 from flask import Blueprint
 
-from app.views import error_views, static_views
+from app.views import error_views, static_views, employee, facility, insurance_companies
 from app.Database import db
-
+from app.views.insurance_companies import add_insurance_company
 
 bp = Blueprint('routes', __name__)
-
-
 
 
 # alias
@@ -18,7 +16,6 @@ def before_request():
 
 @bp.teardown_app_request
 def shutdown_session(response_or_exc):
-
     db.disconnect()
 
 
@@ -29,3 +26,7 @@ bp.register_error_handler(500, error_views.internal_error)
 
 # Public views
 bp.add_url_rule("/", view_func=static_views.index)
+bp.add_url_rule("/add_employee/<job_class>", view_func=employee.add_employee, methods=['GET', 'POST'])
+bp.add_url_rule('/add_facility/<ftype>',view_func=facility.add_facility, methods=['GET', 'POST'])
+bp.add_url_rule('/add_insurance_company',view_func=insurance_companies.add_insurance_company, methods=['GET', 'POST'])
+
