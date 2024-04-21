55407ddcdff6f62644f651a9aa7f629b4699e997
jgwentworth92 <alphabetboi92@protonmail.com>
2024-04-21 13:23:46 -0400

    minor updates to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import logging
 
 # Core Flask imports
 from flask import Flask
@@ -7,17 +8,22 @@ from app import routes
 from app.Database import db, create_facility_tables, create_employee_tables, create_employee_sublcass_tables, \
     create_insurance_tables, create_treats_tables, create_patient_table, create_appointments_tables, \
     create_invoice_tables
+from flask_bootstrap import Bootstrap5
+
+from app.crud import get_all_facility
+from app.utils.common import setup_logging
 
 # App imports
 
 db_manager = db
-
+setup_logging()
 
 def create_app():
     app = Flask(__name__)
-
+    app.config['SECRET_KEY'] = 'your_really_secret_key_here'
     db_manager.connect()
     session = db.get_db()
+    """
     create_facility_tables(session)
     create_employee_tables(session)
     create_employee_sublcass_tables(session)
@@ -26,8 +32,9 @@ def create_app():
     create_treats_tables(session)
     create_appointments_tables(session)
     create_invoice_tables(session)
+    """
 
-
+    bootstrap = Bootstrap5(app)
     app.register_blueprint(routes.bp)
 
     return app
